c99dc6b9b148c2be438ce0b34a7957a72efdeb5c
Lara Lopez Josue Isaac <jilljosue95@gmail.com>
2025-06-02 09:57:13 -0700

    Added useFrameworkReady.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,14 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
-import { SplashScreen } from 'expo-router';
+import { SplashScreen } from 'expo-router'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 // Prevent splash screen from auto-hiding
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  useFrameworkReady();
   const [fontsLoaded, fontError] = useFonts({
     'Inter-Regular': Inter_400Regular,
     'Inter-Bold': Inter_700Bold,
